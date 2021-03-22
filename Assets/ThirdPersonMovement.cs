78407aa2ef8d375328ddf5cfb66d6ab0c82d7b46
Parker Johnstone <parker.a.johnstone@gmail.com>
2021-03-22 01:34:49 -0400

    Knight Movement and Attack1
    Idle, walk, run, jump, and attack1 are in. Having problems with Avatar Mask for Attack1 (trying to get it to blend with walk and run). Cinemachine camera added.

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public Animator anim;
+
+    //public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+
+    float turnSmoothVelocity;
+    private Vector3 velocity;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpHeight;
+
+
+
+    void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            StartCoroutine(Attack1());
+        }
+    }
+
+    private void Move()
+    {
+        isGrounded = Physics.CheckSphere(transform.position, groundCheckDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.localEulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            if (isGrounded)
+            {
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    Walk();
+                }                
+                else if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    Run();
+                }                                    
+            }
+
+            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Idle();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void Idle()
+    {
+        anim.SetFloat("Speed", 0, 0.1f, Time.deltaTime);
+    }
+
+    private void Walk()
+    {
+        anim.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+        moveSpeed = walkSpeed;
+    }
+
+    private void Run()
+    {
+        anim.SetFloat("Speed", 1, 0.1f, Time.deltaTime);
+        moveSpeed = runSpeed;
+    }
+
+    private void Jump()
+    {
+        anim.SetTrigger("Jump");
+        velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+    }
+
+    private IEnumerator Attack1()
+    {
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 1);
+        anim.SetTrigger("Attack1");
+
+        yield return new WaitForSeconds(0.9f);
+        anim.SetLayerWeight(anim.GetLayerIndex("Attack Layer"), 0);
+    }
+}
